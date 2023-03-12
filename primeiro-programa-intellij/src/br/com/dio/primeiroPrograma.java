65796d7fae1e1c7d9985102dd06e6e30ddac0d68
Carlos Reiker <carlos.reiker@hotmail.com>
2023-03-11 22:44:31 -0300

    import package

--- primeiro-programa-intellij/src/br/com/dio/primeiroPrograma.java
+++ primeiro-programa-intellij/src/br/com/dio/primeiroPrograma.java
@@ -1,7 +1,13 @@
 package br.com.dio;
 
+
+import br.com.dio.model.gato;
+
 public class primeiroPrograma {
     public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
 
         /*int a = 5;
         int b = 3;
