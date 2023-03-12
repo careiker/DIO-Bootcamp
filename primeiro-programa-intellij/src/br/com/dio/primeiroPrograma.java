8440e69eb7f743c981b613d691613d944217867f
Carlos Reiker <carlos.reiker@hotmail.com>
2023-03-11 22:26:19 -0300

    second commit

---
+++ primeiro-programa-intellij/src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello world "+ (a+b));
+    }
+}
