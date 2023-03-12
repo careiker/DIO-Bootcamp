5a747e86b8bfc68547a036100437e5c04a5127d6
Carlos Reiker <carlos.reiker@hotmail.com>
2023-03-11 22:33:39 -0300

    comment

--- primeiro-programa-intellij/src/br/com/dio/primeiroPrograma.java
+++ primeiro-programa-intellij/src/br/com/dio/primeiroPrograma.java
@@ -2,8 +2,8 @@ package br.com.dio;
 
 public class primeiroPrograma {
     public static void main(String[] args) {
-        int a = 5;
+        /*int a = 5;
         int b = 3;
-        System.out.println("Hello world "+ (a+b));
+        System.out.println("Hello world "+ (a+b));*/
     }
 }
