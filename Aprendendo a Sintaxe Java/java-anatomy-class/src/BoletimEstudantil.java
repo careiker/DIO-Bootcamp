a7b1d6c6a0139748c5958997750db975b03e61fe
Carlos Reiker <carlos.reiker@hotmail.com>
2023-03-17 22:08:53 -0300

    identação na prática

--- Aprendendo a Sintaxe Java/java-anatomy-class/src/BoletimEstudantil.java
+++ Aprendendo a Sintaxe Java/java-anatomy-class/src/BoletimEstudantil.java
@@ -1,11 +1,11 @@
 public class BoletimEstudantil {
     public static void main(String[] args) {
-        int mediaFinal = 7;
+        int mediaFinal = 5;
         if(mediaFinal<6)
-        System.out.println("Reporvado");
+            System.out.println("Reporvado");
         else if(mediaFinal==6)
-        System.out.println("Prova Minerva");
+            System.out.println("Prova Minerva");
         else
-        System.out.println("Aprovado");
+            System.out.println("Aprovado");
     }
 }
