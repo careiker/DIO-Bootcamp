53362dce95d2650bd4edeef4796cb68097693528
Carlos Reiker <carlos.reiker@hotmail.com>
2023-03-23 22:56:43 -0300

    Update TiposVariaveis.java

--- tipos-e-variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        int numero = 5;
+        /*int numero = 5;
         numero = 10;
 
         final double PI = 3.14;// Não pode ser alterado, é uma constante
 
         System.out.println(numero);
-        System.out.println(PI);
+        System.out.println(PI);*/
+
+        int number = 5;
+
+        number = - number;//adicionar o sinal - torna o int negativo
+        number = + number;// adicionar sinal + torna positivo novamente
+
+        System.out.println(number);
     }
 }
