7c8faecaad563196728b7547168f73ab05a822df
Carlos Reiker <carlos.reiker@hotmail.com>
2023-03-23 22:32:26 -0300

    variable and constant

--- tipos-e-variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        ;
+        int numero = 5;
+        numero = 10;
+
+        final double PI = 3.14;// Não pode ser alterado, é uma constante
+
+        System.out.println(numero);
+        System.out.println(PI);
     }
 }
